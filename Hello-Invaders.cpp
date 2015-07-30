3e6dd2b306453108007497870163698ba16b06e3
twiy-logic <julien@twiy-logic.fr>
2015-07-31 00:57:30 +0200

    Amélioration de indentation du code.

--- Hello-Invaders.cpp
+++ Hello-Invaders.cpp
@@ -102,22 +102,24 @@ void menue()
 	cout << "                   |             Aide ?              |                    " << endl;
 	cout << "                   '---------------------------------'                    " << endl;
 
-	color(12);
+	color(12); 
 	GotoXY(12, 10); cout << "-->";
+
 	int fleche(1);
 	char touche(_getch());
 
 	while (touche != ok)
 	{
 
-		if (touche == up){ fleche--; }
-		if (touche == down){ fleche++; }
+		if (touche == up)	{ fleche--; }
+		if (touche == down)	{ fleche++; }
 
-		if (fleche == 0){ fleche = 2; }
-		if (fleche == 3){ fleche = 1; }
+		if (fleche == 0)	{ fleche = 2; }
+		if (fleche == 3)	{ fleche = 1; }
 		/// la nouvelle valeur de la fleche a été donné
 
 		color(12);
+
 		if (fleche == 1){
 			GotoXY(12, 10); cout << "-->";
 			GotoXY(12, 14); cout << "   ";
@@ -137,19 +139,19 @@ void menue()
 	ClearScreen();
 
 	// on n'a pas besoin de preciser ce qui ce passe si fleche == 1
-	if (fleche == 2){ ecrans_help(); }
-	else if (fleche == 1){ fond_ecran(); game(); }
+	if		(fleche == 2)	{ ecrans_help(); }
+	else if (fleche == 1)	{ fond_ecran(); game(); }
 
 	color(9);
-
 } ///fin de la fonction menu
 
-/*****************************************
+/****************************************
 *  c'est le premier ecran qui apparait  *
 *****************************************/
 void pressentation(){
 
 	color(10);
+
 	cout << endl;
 	cout << "                    _   _   _____   _       _       _____   " << endl;
 	cout << "                   | | | | |  ___| | |     | |     /  _  1  " << endl;
@@ -165,25 +167,28 @@ void pressentation(){
 	cout << "       _| |_  | | 1  | | |/ /   / ,__  | | |_| | | |___  | | 1 1   ___| | " << endl;
 	cout << "      |_____| |_|  1_| |___/   /_/   |_| |_____/ |_____| |_|  1_1 /_____/ " << endl;
 	cout << endl << endl << endl; color(9);
-	cout << "                          Programme par julien jacob      " << endl << endl;
-	cout << "                                 twiy-logic.fr            " << endl << endl << endl;
-	cout << "                       Tapez sur une touche pour continuer" << endl;
+	cout << "                          Programme par julien jacob       " << endl << endl;
+	cout << "                                 twiy-logic.fr             " << endl << endl << endl;
+	cout << "                       Tapez sur une touche pour continuer " << endl;
+	
 	fflush(stdout);
 	_getch();
 	ClearScreen();
 }
 
-/*******************************************************
+/*****************************************************
 *  ecran qui affiche l'aide et renvoie vers le menu  *
-*******************************************************/
+******************************************************/
 void ecrans_help(){
+
 	color(9);
+
 	cout << endl;
 	cout << "                     Bienvenue dans HELLO INVADERS,                       " << endl << endl;
 	cout << "     Le but de ce jeu est de detruire le plus d'asteroides possible       " << endl << endl;
 	cout << "       -  Si un asteroide franchit la frontiere, vous perdez une vie      " << endl << endl;
 	cout << "       -  si vous detruisez un asteroide, vous gagnez 10 points           " << endl << endl << endl;
-	cout << "                              ,---------------,       " << endl;
+	cout << "                              ,---------------,        " << endl;
 	cout << "                              | les touches : |        " << endl;
 	cout << "                              '---------------'        " << endl << endl;
 	cout << "                              Up   :   'Fleche Haut'   " << endl << endl;
@@ -198,13 +203,16 @@ void ecrans_help(){
 	menue();
 }
 
-/******************************************************
+/*****************************************************
 *  ecran qui s'affiche quand les vies sont epuisées  *
 *               renvoi ver le menu                   *
 ******************************************************/
 void game_over(int score){
+
 	ClearScreen();
+
 	color(12);
+
 	cout << endl;
 	cout << "         _____       ___       ___  ___   _____  " << endl;
 	cout << "        /  ___|     /   |     /   |/   | | ____| " << endl;
@@ -230,14 +238,15 @@ void game_over(int score){
 	menue();
 }
 
-/***************************************
-*  dessine le fond d'ecran du jeu   *
-***************************************/
+/************************************
+*  dessine le fond d'ecran du jeu	*
+*************************************/
 void fond_ecran()
 {
 	color(9);
+
 	cout << "  ______________________________________________________          " << endl;
-	cout << " .-----------------------------------------------------.|        " << endl;
+	cout << " .-----------------------------------------------------.|         " << endl;
 	cout << " |                                                     ||         " << endl;
 	cout << " |                                                     ||  Score :" << endl;
 	cout << " |                                                     ||         " << endl;
@@ -258,29 +267,23 @@ void fond_ecran()
 	cout << " '-----------------------------------------------------'          " << endl;
 }
 
-/************************************************
+/***********************************************
 *  affiche un message 'pause', stoppe le jeu   *
 * tant qu'une touche n'est pas tapée et        *
 * efface le message                            *
 ************************************************/
 void jeux_pause()
 {
-	GotoXY(10, 10);
-	cout << " .-----------. " << endl;
-	GotoXY(10, 11);
-	cout << " |   PAUSE   | " << endl;
-	GotoXY(10, 12);
-	cout << " '-----------' " << endl;
+	GotoXY(10, 10);		cout << " .-----------. " << endl;
+	GotoXY(10, 11);		cout << " |   PAUSE   | " << endl;
+	GotoXY(10, 12);		cout << " '-----------' " << endl;
 
 	fflush(stdout);
 	_getch();
 
-	GotoXY(10, 10);
-	cout << "               " << endl;
-	GotoXY(10, 11);
-	cout << "               " << endl;
-	GotoXY(10, 12);
-	cout << "               " << endl;
+	GotoXY(10, 10);		cout << "               " << endl;
+	GotoXY(10, 11);		cout << "               " << endl;
+	GotoXY(10, 12);		cout << "               " << endl;
 }
 
 void game()
@@ -305,17 +308,17 @@ void game()
 			position += 2; GotoXY(axe_veseau, position); cout << "    "; position -= 2; 
 			
 			/*************************** 			 
-			**  code des asteroides 1 ** 			 
+			*   code des asteroides 1  * 			 
 			****************************/	 			
 			if (compteur > 5)
 			{
-				if (cycle == 1)			  { axe_asteroide = 10; }
+				if		(cycle == 1)	{ axe_asteroide = 10; }
 				else if (cycle == 2)    { axe_asteroide = 16; }
-				else if (cycle == 3)    { axe_asteroide = 9; }
+				else if (cycle == 3)    { axe_asteroide = 9;  }
 				else if (cycle == 4)    { axe_asteroide = 12; }
 				else if (cycle == 5)    { axe_asteroide = 15; }
-				else if (cycle == 6)    { axe_asteroide = 8; }
-				else if (cycle == 7)    { axe_asteroide = 6; }
+				else if (cycle == 6)    { axe_asteroide = 8;  }
+				else if (cycle == 7)    { axe_asteroide = 6;  }
 				else if (cycle == 8)    { axe_asteroide = 10; cycle = 1; }
 
 				GotoXY(compteur, axe_asteroide);
@@ -332,7 +335,7 @@ void game()
 			}
 
 			/***************************
-			**     code des tirs      **
+			*      code des tirs       *
 			****************************/
 			color(14);
 			if (v_shoot1 == true) // efface de rayon de tir
@@ -354,11 +357,11 @@ void game()
 			}
 
 			/***************************
-			**  compteur et cycles    **
+			*   compteur et cycles     *
 			****************************/
 
-			if (score > 50)	{ v_sleep = 50; }
-			if (score > 80)	{ v_sleep = 30; }
+			if (score > 50)		{ v_sleep = 50; }
+			if (score > 80)		{ v_sleep = 30; }
 			if (score > 100)	{ v_sleep = 25; }
 			if (score > 150)	{ v_sleep = 20; }
 			if (score > 300)	{ v_sleep = 15; }
@@ -367,22 +370,19 @@ void game()
 			if (mode_programmeur == true) { afficher_mode_programmeur(v_sleep, compteur, cycle); }
 
 			compteur++;
-			if (compteur > 50){ compteur = 0; cycle++; }
+			if (compteur > 50)	{ compteur = 0; cycle++; }
 			Sleep(v_sleep);
 
 			/***************************
-			**   score et resultats   **
+			*    score et resultats    *
 			****************************/
 			color(11);
-			GotoXY(70, 3);
-			cout << "         ";
-			GotoXY(70, 3);
-			cout << score;
 
-			GotoXY(70, 5);
-			cout << "         ";
-			GotoXY(70, 5);
-			cout << vie;
+			GotoXY(70, 3);	cout << "         ";
+			GotoXY(70, 3);	cout << score;
+
+			GotoXY(70, 5);	cout << "         ";
+			GotoXY(70, 5);	cout << vie;
 
 			afficher_coeurs(vie);
 
@@ -392,13 +392,13 @@ void game()
 		v_shoot = false;
 
 		///on relie la touche avec ces actions
-		if (touche == up)			{ position--; }
+		if		(touche == up)		{ position--; }
 		else if (touche == down)	{ position++; }
 		else if (touche == shoot)	{ v_shoot = true; }
 		else if (touche == ok)		{ jeux_pause(); }
 		else if (touche == 'a')
 		{
-			if (mode_programmeur == true)	{ mode_programmeur = false; }
+			if		(mode_programmeur == true)	{ mode_programmeur = false; }
 			else if (mode_programmeur == false) { mode_programmeur = true; }
 		}
 
@@ -448,18 +448,12 @@ void afficher_coeurs(int nb_vie)
 // joueurs mais qui peuvent etre utile pour surveiller le programme
 void afficher_mode_programmeur(int v_sleep, int compteur, int cycle)
 {
-	GotoXY(75, 23);
-	cout << "         ";
-	GotoXY(75, 23);
-	cout << v_sleep;
+	GotoXY(75, 23);		cout << "         ";
+	GotoXY(75, 23);		cout << v_sleep;
 
-	GotoXY(75, 19);
-	cout << "         ";
-	GotoXY(75, 19);
-	cout << compteur;
+	GotoXY(75, 19);		cout << "         ";
+	GotoXY(75, 19);		cout << compteur;
 
-	GotoXY(75, 21);
-	cout << "         ";
-	GotoXY(75, 21);
-	cout << cycle;
+	GotoXY(75, 21);		cout << "         ";
+	GotoXY(75, 21);		cout << cycle;
 }
